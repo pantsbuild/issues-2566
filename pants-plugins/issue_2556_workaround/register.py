6f1e60163fa59ca81bdd345ff9a91c7d715bd199
John Sirois <john.sirois@gmail.com>
2015-11-15 14:55:14 -0700

    Minimize the example further.
    There were un-needed BUILD.tools entries, replace with Aurora's which
    includes a pointer to the issue tracking the need for the BUILD.tools
    boilerplate.  Also cleanup the custom plugin slightly.

--- pants-plugins/issue_2556_workaround/register.py
+++ pants-plugins/issue_2556_workaround/register.py
@@ -5,9 +5,9 @@ from pants.build_graph.build_file_aliases import BuildFileAliases
 def python_tests(parse_context, dependencies=None, **kwargs):
   pytest_timeout_req = parse_context.create_object('python_requirement',
                                                    requirement='pytest-timeout<1')
-  prl = parse_context.create_object('python_requirement_library', 
-                                    name='_issue_2556_workaround_',
-                                    requirements=[pytest_timeout_req])
+  parse_context.create_object('python_requirement_library',
+                              name='_issue_2556_workaround_',
+                              requirements=[pytest_timeout_req])
   dependencies = dependencies or []
   dependencies.append(':_issue_2556_workaround_')
   parse_context.create_object(PythonTests, dependencies=dependencies, **kwargs)
